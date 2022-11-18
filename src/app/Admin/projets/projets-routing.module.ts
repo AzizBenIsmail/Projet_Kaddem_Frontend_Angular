f9416c670ed32f84b056aaef071756eeb02bb538
Eya-BenAmor <61431876+Eya-BenAmor@users.noreply.github.com>
2022-11-18 02:34:07 +0100

    CRUD-PROJET

--- src/app/Admin/projets/projets-routing.module.ts
+++ src/app/Admin/projets/projets-routing.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AddProjetComponent } from './add-projet/add-projet.component';
-import { AddTacheComponent } from './add-tache/add-tache.component';
 import { GetProjetsComponent } from './get-projets/get-projets.component';
-import { GetTachesComponent } from './get-taches/get-taches.component';
-import { ProjetResolver } from './ProjetResolver';
 import { UpdateProjetComponent } from './update-projet/update-projet.component';
-import { UpdateTacheComponent } from './update-tache/update-tache.component';
+
+
 
 const routes: Routes = [
 
--- src/app/Admin/projets/projets.module.ts
+++ src/app/Admin/projets/projets.module.ts
@@ -2,13 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule, DatePipe } from '@angular/common';
 
 import { ProjetsRoutingModule } from './projets-routing.module';
-import { UpdateProjetComponent } from './update-projet/update-projet.component';
+
 import { UpdateTacheComponent } from './update-tache/update-tache.component';
 import { GetTachesComponent } from './get-taches/get-taches.component';
-import { GetProjetsComponent } from './get-projets/get-projets.component';
-import { AddProjetComponent } from './add-projet/add-projet.component';
+
 import { AddTacheComponent } from './add-tache/add-tache.component';
-import { AdminLayoutModule } from 'app/layouts/admin-layout/admin-layout.module';
+
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatRippleModule } from '@angular/material/core';
@@ -16,18 +15,22 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatTooltipModule } from '@angular/material/tooltip';
-import { NewDirectDirective } from './new-direct.directive';
+
+import { UpdateProjetComponent } from './update-projet/update-projet.component';
+import { GetProjetsComponent } from './get-projets/get-projets.component';
+import { AddProjetComponent } from './add-projet/add-projet.component';
 
 
 
 @NgModule({
   declarations: [
-    UpdateProjetComponent,
+  
     UpdateTacheComponent,
     GetTachesComponent,
     GetProjetsComponent,
     AddProjetComponent,
     AddTacheComponent,
+    UpdateProjetComponent,
   
   ],
   imports: [
