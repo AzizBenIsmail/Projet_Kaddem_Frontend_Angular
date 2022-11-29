989125daaacdb492a343608e398e3b1a1818ffb7
Mohamed Aziz Ben Ismail <aziz.270700@gmail.com>
2022-11-29 21:45:52 +0100

    Update projets.module.ts

--- src/app/Admin/projets/projets.module.ts
+++ src/app/Admin/projets/projets.module.ts
@@ -1,13 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule, DatePipe } from '@angular/common';
-
 import { ProjetsRoutingModule } from './projets-routing.module';
-
 import { UpdateTacheComponent } from './update-tache/update-tache.component';
 import { GetTachesComponent } from './get-taches/get-taches.component';
-
 import { AddTacheComponent } from './add-tache/add-tache.component';
-
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatRippleModule } from '@angular/material/core';
@@ -15,7 +11,6 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatTooltipModule } from '@angular/material/tooltip';
-
 import { UpdateProjetComponent } from './update-projet/update-projet.component';
 import { GetProjetsComponent } from './get-projets/get-projets.component';
 import { AddProjetComponent } from './add-projet/add-projet.component';
