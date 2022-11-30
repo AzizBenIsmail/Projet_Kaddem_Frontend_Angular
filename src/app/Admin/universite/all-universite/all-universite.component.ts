4beab96069020569bda82833262ca8a4366c3897
Mohamed Aziz Ben Ismail <aziz.270700@gmail.com>
2022-11-30 19:55:03 +0100

    up

--- src/app/Admin/universite/all-universite/all-universite.component.ts
+++ src/app/Admin/universite/all-universite/all-universite.component.ts
@@ -1,6 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject, OnInit } from '@angular/core';
+import { left } from '@popperjs/core';
 import { Universite } from 'app/models/Universite';
 import { UniversiteserviceService } from 'app/Services/ServicesUniversite/universiteservice.service';
+declare var $: any;
+
 
 @Component({
   selector: 'app-all-universite',
@@ -9,7 +13,7 @@ import { UniversiteserviceService } from 'app/Services/ServicesUniversite/univer
 })
 export class AllUniversiteComponent implements OnInit {
 universites:any;
-  constructor(private universiteService:UniversiteserviceService) { }
+  constructor(private universiteService:UniversiteserviceService,@Inject(DOCUMENT) private doc: Document) { }
 
   ngOnInit(): void {
     this.universiteService.findAllUniversite().subscribe(
@@ -20,4 +24,44 @@ this.universites=d;
     )
   }
 
+  refresh():void {
+    this.doc.defaultView.location.reload();
+    this.showNotification("top","left");
+}
+
+showNotification(from, align){
+  const type = ['','info','success','warning','danger'];
+
+  const color = Math.floor((Math.random() * 4) + 1);
+
+  $.notify({
+      icon: "notifications",
+      message: "Welcome to <b>Material Dashboard</b> - a beautiful freebie for every web developer."
+
+  },{
+      type: type[color],
+      timer: 4000,
+      placement: {
+          from: from,
+          align: align
+      },
+      template: '<div data-notify="container" class="col-xl-4 col-lg-4 col-11 col-sm-4 col-md-4 alert alert-{0} alert-with-icon" role="alert">' +
+        '<button mat-button  type="button" aria-hidden="true" class="close mat-button" data-notify="dismiss">  <i class="material-icons">close</i></button>' +
+        '<i class="material-icons" data-notify="icon">notifications</i> ' +
+        '<span data-notify="title">{1}</span> ' +
+        '<span data-notify="message">{2}</span>' +
+        '<div class="progress" data-notify="progressbar">' +
+          '<div class="progress-bar progress-bar-{0}" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0%;"></div>' +
+        '</div>' +
+        '<a href="{3}" target="{4}" data-notify="url"></a>' +
+      '</div>'
+  });
+}
+onPrint(divName) {
+  const printContents = document.getElementById(divName).innerHTML;
+  const originalContents = document.body.innerHTML;
+  document.body.innerHTML = printContents;
+  window.print();
+  document.body.innerHTML = originalContents;
+}
 }
