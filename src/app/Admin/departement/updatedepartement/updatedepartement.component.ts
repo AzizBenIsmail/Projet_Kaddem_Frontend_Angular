7c17ec9c8740421a9612ba0f18c82689b1e82000
Mohamed Aziz Ben Ismail <aziz.270700@gmail.com>
2022-12-05 22:41:20 +0100

    update département
    te

--- src/app/Admin/departement/updatedepartement/updatedepartement.component.ts
+++ src/app/Admin/departement/updatedepartement/updatedepartement.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import {DepartementserviceService} from '../../../Services/ServicesDepartement/departementservice.service';
+import {ActivatedRoute, Router} from '@angular/router';
+import {Departement} from '../../../models/Departement';
 
 @Component({
   selector: 'app-updatedepartement',
@@ -7,9 +11,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UpdatedepartementComponent implements OnInit {
 
-  constructor() { }
+
+  departements: Departement[] = [];
+  departement: Departement = new Departement();
+
+  constructor(private departementservice: DepartementserviceService, private _routerAdd: Router,
+              private _activatedRoute: ActivatedRoute, private fb: FormBuilder) { }
 
   ngOnInit(): void {
+  }
+  DepartementForm = new FormGroup({
+    nomDepartement: new FormControl('',[Validators.required, Validators.minLength(3)]),
+    nombre_classes: new FormControl(''),
+    nombre_detage: new FormControl(''),
+  });
+
+  onSubmit() {
+    const id = +this._activatedRoute.snapshot.paramMap.get('id');
+    this.departement=this.DepartementForm.value;
+    this.departementservice.updateDepartement(id,this.departement).subscribe(
+        data => {
+          console.log('response', data);
+          this._routerAdd.navigateByUrl("Departement");
+        })
+    console.log(this.departement);
   }
 
 }
